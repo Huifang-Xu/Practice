c58dee9ca812aedb0a3c4973b8e02769a32ba51d
Huifang-Xu <91753987+Huifang-Xu@users.noreply.github.com>
2021-10-12 01:20:15 +0800

    Update pheno_processing.R

--- pheno_processing.R
+++ pheno_processing.R
@@ -2,7 +2,6 @@ R: processing ICD-10 and Sex information
 library(tidyverse)
 #read data
 
-
 #R function manyColsToDummy
 manyColsToDummy<-function(search_terms, search_columns,
                           output_table){
@@ -77,24 +76,18 @@ df_join <- as_tibble(inner_join(df_ukb34137, df_ukb37330,
 #extract data: sex (code: f.31.0.0), BMI (code: f.21001.0.0)
 df_sex_BMI <- select(df_join,f.eid,f.31.0.0,f.21001.0.0)
 #extract BMI and sex colums
-#read data
-df_ukb34137 <- read.table("ukb34137.tab",header=TRUE, sep="\t")
-df_ukb37330 <- read.table("ukb37330.tab",header=TRUE, sep="\t")
-df_join <- as_tibble(inner_join(df_ukb34137, df_ukb37330, 
-                                by=intersect(colnames(df_ukb34137), colnames(df_ukb37330))))
-df_sex_BMI <- select(df_join,f.eid,f.31.0.0,f.21001.0.0)
 
 #count the number of female and male, median BMI of female and male
 num_female <- nrow(subset(df,f.31.0.0==0))
 num_male <- nrow(subset(df,f.31.0.0==1))
-female_median <- median(subset(df_sex_BMI,f.31.0.0==0)$f.21001.0.0,na.rm = TRUE)
-male_median <- median(subset(df_sex_BMI,f.31.0.0==1)$f.21001.0.0,na.rm = TRUE)
+female_median_BMI <- median(subset(df_sex_BMI,f.31.0.0==0)$f.21001.0.0,na.rm = TRUE)
+male_median_BMI <- median(subset(df_sex_BMI,f.31.0.0==1)$f.21001.0.0,na.rm = TRUE)
 
 #boxplot
 library(ggplot2)
 annotation <- data.frame(x=c(1.5,2.5,1,2),
-                         y=c(female_median,male_median,78,78),
-                         label=c(female_median,male_median,paste("n = ",num_female),paste("n = ",num_male)))
+                         y=c(female_median_BMI,male_median_BMI,78,78),
+                         label=c(female_median_BMI,male_median_BMI,paste("n = ",num_female),paste("n = ",num_male)))
 pdf("sex_BMI.pdf",width = 6,height = 8)
 ggplot(df_sex_BMI,aes(x=factor(f.31.0.0),y=f.21001.0.0))
   +geom_boxplot(aes(fill=factor(f.31.0.0,levels = c("0", "1"),labels = c("Female","Male"))))
